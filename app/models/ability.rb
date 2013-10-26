209428a8c155bc633bcdc0a088648e424f7b57ca
Coto Cisternas <cotocisternas@gmail.com>
2013-10-25 22:52:48 -0300

    ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,31 +2,24 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user 
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. 
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+    user ||= User.new # guest user (not logged in)
+    if user.has_role? :admin
+      can :manage, :all
+    end
+    if user.has_role? :user
+      # Dashboard Controller
+      can [:index,:show], :dashboard
+      # Directory Controller
+      can [:index, :show], :directory
+      # User Controller
+      can [:edit,:update], User, :id => user.id
+      can [:index,:show], User
+      # Extension Controller
+      # can :read, Extension, :user_id => user.id
+      # Cdr Controller
+      can :read, Cdr, :src => user.extension.name if user.extension
+    end
   end
+  
 end
+    
